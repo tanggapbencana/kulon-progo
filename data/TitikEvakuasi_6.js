41fa3e241af49a2570eac546df3e80a0ddee5dfb
Dhany Yudi Prasetyo <dhanyyudi@users.noreply.github.com>
2020-11-01 05:54:10 +0700

    Upload
    Upload All

---
+++ data/TitikEvakuasi_6.js
@@ -0,0 +1,15 @@
+var json_TitikEvakuasi_6 = {
+"type": "FeatureCollection",
+"name": "TitikEvakuasi_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nama": "Titik Evakuasi 1", "Kapasitas": "Tenda darurat 500-800 orang" }, "geometry": { "type": "Point", "coordinates": [ 110.10229551746643, -7.780281347770783 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Nama": "Titik Evakuasi 2", "Kapasitas": "Kawasan Masjid Kapasitas 800-1000 orang" }, "geometry": { "type": "Point", "coordinates": [ 110.07533054559282, -7.823843299874605 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Nama": "Titik Evakuasi 3", "Kapasitas": "200-500 Orang" }, "geometry": { "type": "Point", "coordinates": [ 110.135439571456132, -7.728440651447369 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Nama": "Titik Evakuasi 4", "Kapasitas": "200-500 orang" }, "geometry": { "type": "Point", "coordinates": [ 110.158096179303357, -7.704106386262705 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Nama": "Titik Evakuasi 5", "Kapasitas": "400-600 Orang" }, "geometry": { "type": "Point", "coordinates": [ 110.167396830965345, -7.68120770223244 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Nama": "Titik Evakuasi 6", "Kapasitas": "200-500 Orang" }, "geometry": { "type": "Point", "coordinates": [ 110.252880140840688, -7.689502157387536 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Nama": "Titik Evakuasi 7", "Kapasitas": "200-400 Orang" }, "geometry": { "type": "Point", "coordinates": [ 110.227728829141256, -7.683446029552969 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Nama": "Titik Evakuasi 8", "Kapasitas": "200-500 Orang" }, "geometry": { "type": "Point", "coordinates": [ 110.203667200091104, -7.684909142876736 ] } }
+]
+}
---
+++ data/TitikSumberGempaBumi_4.js
@@ -0,0 +1,8 @@
+var json_TitikSumberGempaBumi_4 = {
+"type": "FeatureCollection",
+"name": "TitikSumberGempaBumi_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Lintang": -8.4, "Bujur": 108.92326 }, "geometry": { "type": "Point", "coordinates": [ 108.92326, -8.4 ] } }
+]
+}
